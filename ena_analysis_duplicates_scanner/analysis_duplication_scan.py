609b55e98a7d82beec4d4f92a25db8111285a8a6
azyoud <azyoud@ebi.ac.uk>
2024-03-25 15:02:45 +0000

    Update analysis_duplication_scan.py

--- ena_analysis_duplicates_scanner/analysis_duplication_scan.py
+++ ena_analysis_duplicates_scanner/analysis_duplication_scan.py
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+__author__ = "Ahmad Zyoud"
+
 import argparse, hashlib, os, subprocess, sys, time
 import sys
 import shlex
@@ -29,6 +31,7 @@ import numpy as np
 import pandas as pd
 import datetime
 import configparser
+import fnmatch
 
 
 parser = argparse.ArgumentParser(prog='duplication_scan.py', formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -78,6 +81,7 @@ def fetch_details(connection):
     config = configparser.ConfigParser()
     config.read(args.config)
     sys.stderr.write("Processing Stage 1 ..........\n")
+    sys.stderr.write("scan for files duplication ..........\n")
     c = connection.cursor()
     dict = {}
     value =[]
@@ -90,7 +94,51 @@ def fetch_details(connection):
     dict['file_name'] = value
     df = pd.DataFrame.from_dict (dict, orient='columns', dtype=None, columns=None)
 
+    return df
 
+def tarFiles_duplicates_calling (connection):
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    sys.stderr.write("PATHOGEN_ANALYSIS type files fetching ..........\n")
+    c = connection.cursor()
+
+    value = []
+    c.execute(
+        f"select run_id, a.analysis_id, a.status_id, a.first_created, a.audit_time, wf.UPLOAD_FILE_PATH, a.bioproject_id, wf.bytes, wf.checksum from WEBIN_FILE wf join\
+    (select ANALYSIS_ID, STATUS_ID, FIRST_CREATED,AUDIT_TIME, BIOPROJECT_ID, extractValue(ANALYSIS_XML, 'ANALYSIS_SET/ANALYSIS/RUN_REF/IDENTIFIERS/PRIMARY_ID') as run_id\
+    from analysis where bioproject_id in ({config['DataHub_Projects']['projects']})\
+    and status_id !=5 and STATUS_ID !=3 and ANALYSIS_TYPE = 'PATHOGEN_ANALYSIS') a on wf.DATA_FILE_OWNER_ID = a.ANALYSIS_ID"
+    )
+    for row in c:
+        value.append(row)
+    df = pd.DataFrame(value)
+    df.columns = ['run_id', 'Accession', 'Status_id', 'First_created', 'Audit_time', 'File_name', 'Project_id', 'File_Size', 'Checksum']
+
+    return df
+
+
+def tarFiles_duplicates_processing(df):
+    sys.stderr.write("PATHOGEN_ANALYSIS type files fetching ..........\n")
+    df_grouped = df.groupby('run_id').size()
+    df_filtered = df_grouped[df_grouped > 3].reset_index(name='counts')
+    df_merged = df_filtered.merge(df, how= 'inner', on= 'run_id')
+    df_merged = df_merged.drop('counts', axis=1)
+    return (df_merged)
+
+def tarFiles_processing (df_merged):
+    sys.stderr.write("duplicated Tar files fetching ..........\n")
+    tarFile_list =[]
+    for index, row in df_merged.iterrows():
+        if fnmatch.fnmatch(row['File_name'], '*tar.gz'):
+            tarFile_list.append(row)
+    tarFile_df = pd.DataFrame(tarFile_list)
+    tarFile_df.columns = ['run_id', 'Accession', 'Status_id', 'First_created', 'Audit_time', 'File_name', 'Project_id', 'File_Size', 'Checksum']
+
+    return tarFile_df
+
+
+def stage2_processing(connection, df):
+    c = connection.cursor()
     sys.stderr.write("Processing Stage 2 ..........\n")
     f = open(f"{outdir}/duplicated_analysis_details.log.txt", "w")
     header = "\t".join(['Accession', 'Status_id', 'First_created', 'Audit_time', 'File_name', 'Project_id', 'File_Size', 'Checksum'])
@@ -139,10 +187,10 @@ def drop_duplicate(df):
     if not df_filtered.empty:
         df_processed = df_filtered.sort_values('First_created').drop_duplicates('File_name', keep='last')
         df_processed.to_csv(f'{outdir}/duplicates_to_be_suppressed.log.txt', index=False, sep='\t', mode='w')
-        sys.stderr.write(f"Public Duplicated Data were Found, please check {outdir}/duplicates_to_be_suppressed.log.txt")
+        sys.stderr.write(f"{len(df_processed)} Public Duplicated Data were Found, please check {outdir}/duplicates_to_be_suppressed.log.txt")
     if not cancelled_df.empty:
         cancelled_df.to_csv(f'{outdir}/duplicates_to_be_cancelled.log.txt', index=False, sep='\t', mode='w')
-        sys.stderr.write(f"Private Duplicated Data were Found, please check {outdir}/duplicates_to_be_cancelled.log.txt")
+        sys.stderr.write(f"{len(cancelled_df)} Private Duplicated Data were Found, please check {outdir}/duplicates_to_be_cancelled.log.txt")
     sys.stderr.write("........................ END ...................\n")
 
 #############
@@ -155,7 +203,15 @@ outdir = create_outdir()
 sys.stderr.write("Connecting to ERAPRO...\n")
 db_conn = setup_connection()
 sys.stderr.write("Querying ERAPRO ..........\n")
-fetch_details(db_conn)
+df = fetch_details(db_conn)
+stage2_processing(db_conn,df)
+tarfiles_df = tarFiles_duplicates_calling(db_conn)
+tarFiles_merged = tarFiles_duplicates_processing(tarfiles_df)
+tarFiles_merged.to_csv(f"{outdir}/duplicated_analysis_details_PATHOGEN_ANALYSIS.log.txt", index=False)
+tarFiles_processed = tarFiles_processing(tarFiles_merged)
+if not tarFiles_processed.empty:
+    tarFiles_processed.to_csv(f'{outdir}/tarFiles_duplicates_to_be_suppressed.log.txt', index=False)
+    sys.stderr.write(f"{len(tarFiles_processed)} Tar Files Duplicated Data were Found, please check {outdir}/tarFiles_duplicates_to_be_suppressed.log.txt\n")
 duplicat_details =pd.read_csv (f"{outdir}/duplicated_analysis_details.log.txt", sep="\t", header=0)
 drop_duplicate(duplicat_details)
 
